51d606b5573c8032d14f92f2cb197539b713d937
Horacio Ibrahim <horacioibrahim@gmail.com>
2014-12-31 01:28:12 -0200

    Feedback 360 com suporte FB

--- feedback360/views.py
+++ feedback360/views.py
@@ -5,4 +5,5 @@ from django.shortcuts import render
 def home(request):
 	return render(request, 'feedback360/home.html')
 
-
+def test_fb(request):
+	return render(request, 'feedback360/test_fb.html')
