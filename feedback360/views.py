1720d66c99b38d1687ac09ae75f4511980154ca5
Horacio Ibrahim <horacioibrahim@gmail.com>
2015-01-01 13:52:28 -0200

    Added page remove to social plugins from Facebook

--- feedback360/views.py
+++ feedback360/views.py
@@ -1,29 +1,32 @@
 from django.shortcuts import render
 
 
-
 def home(request):
-	return render(request, 'feedback360/home.html')
+    return render(request, 'feedback360/home.html')
+
 
 def test_fb(request):
-	return render(request, 'feedback360/test_fb.html')
+    return render(request, 'feedback360/test_fb.html')
+
 
 def access(request):
+    name = None
+    email = None
 
-	name = None
-	email = None	
+    if request.method == "POST":
+        name = request.POST.get('name', None)
+        email = request.POST.get('email', None)
 
-	if request.method == "POST":
-		name = request.POST.get('name', None)
-		email = request.POST.get('email', None)
+        if check_sent(email):
+            pass # If true -> Thanks
+        else:
+            pass # redirect questions or return JSON, etc.
 
-		if check_sent(email):
-			pass # If true -> Thanks
-		else:
-			pass # redirect questions or return JSON, etc.
-
-	# not return here is need
-	return render(request, 'feedback360/test_fb.html', dict(u=username))	
+    # not return here is need
+    return render(request, 'feedback360/test_fb.html', dict(u=username))
 
 def check_sent(email):
-	pass
+    pass
+
+def remove(request):
+    return render(request, 'feedback360/remove.html')
